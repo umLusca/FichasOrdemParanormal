6b01af2c56d127ce85566895324f8a9b45bf34c1
Lucas <ludafepi123456@gmail.com>
2022-09-20 03:11:59 -0400

    1.0

--- test.php
+++ test.php
@@ -3,11 +3,31 @@ require_once "./config/includes.php";
 header('Content-Type: application/json');
 
 
+function verificarcpf(string $cpf): bool
+{
+	$cn = str_split($cpf);
+	$cv = [];
+	$csv = 0;
+	for ($i = 0; $i < 9; $i++) {
+		$cv[$i] = $cn[$i] * ($i + 1);
+	}
+	foreach ($cv as $somando) {
+		$csv += $somando;
+	}
+	$dv1 = ($csv % 11 == 10) ? 0 : ($csv % 11); //Primeiro digito verificador
+	$cv = [];
+	$cn[9] = $dv1;
+	$csv = 0;
+	for ($i = 0; $i < 10; $i++) {
+		$cv[$i] = $cn[$i] * $i;
+	}
+	foreach ($cv as $somando) {
+		$csv += $somando;
+	}
+	$dv2 = ($csv % 11 == 10) ? 0 : ($csv % 11); //segundo digito verificador
+	$dv = $dv1 . $dv2; // digito verificador
+	$cdv = $cn[9] . $cn[10]; // digito verificador do cpf dado
+	return ($cdv === $dv);
+}
 
 
-alert
-
-
-
-
-?>
