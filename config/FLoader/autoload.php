76851b4fafbcf7e4219235d8b9edcfb5b5c3746b
Lucas Pinheiro <ludafepi123456@gmail.com>
2025-01-24 14:10:19 -0400

    Config

--- config/FLoader/autoload.php
+++ config/FLoader/autoload.php
@@ -1,13 +1,24 @@
 <?php
 $directory = ROOT."API/functions/";
 $_loadedFunctions = [];
+
+
 if (is_dir($directory)) {
-    $scan = scandir($directory);
-    foreach ($scan as $file) {
-        $filepath = realpath($directory.$file);
-        if (file_exists($filepath) && str_starts_with($file, "_") && str_ends_with($file, ".php")) {
-            $_loadedFunctions[] = (string) $file;
-            include_once($directory . $file);
-        }
-    }
+	$scan = scandir($directory);
+	foreach ($scan as $file) {
+		$filepath = realpath($directory . $file);
+		if (file_exists($filepath) && str_starts_with($file, "_") && str_ends_with($file, ".php")) {
+			$functionName = substr($file, 1,-4);
+			$_functionsNames[] = (string)$functionName;
+			$_detectedFunctions[$functionName] = (string)$filepath;
+			
+		}
+	}
+}
+
+foreach ($_detectedFunctions as $name => $filepath) {
+	if (file_exists($filepath)) {
+		$_loadedFunctions[] = (string)$name;
+		include_once($filepath);
+	}
 }
